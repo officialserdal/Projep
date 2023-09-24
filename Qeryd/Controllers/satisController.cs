828e9e8bfccc1c870bbaa7aa6657dd4514406d14
Serdal58 <sserdalyildirim58@gmail.com>
2023-09-24 23:03:45 +0300

    .
    .

---
+++ Qeryd/Controllers/satisController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Qeryd.Controllers
+{
+    public class satisController : Controller
+    {
+        // GET: satis
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
